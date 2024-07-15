ace5f445f056fada55540cf95334b2551291022d
GalaletsangM <preciousmafoko@gmail.com>
2024-07-15 09:48:36 -0500

    created weather info component

--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormettedDate";
+import WeatherInfor from "./WeatherInfo";
+
 
 export default function Weather(props) {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
-  const [humidity, setHumidity] = useState(null);
-  const [wind, setWind] = useState(null);
-  const [condition, setCondition] = useState(null);
-  const [date, setDate] = useState(null);
-  const [icon, setIcon] = useState(null);
+  let [weatherData, setWeatherData] = useState({ready : false });
 
   function handleResponse(response) {
-    setTemperature(response.data.main.temp);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-    setCondition(response.data.weather[0].description);
-    setReady(true);
-    setDate(new Date(response.data.dt * 1000));
-    setIcon("https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png");
+    setWeatherData ({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      condition: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      city: response.data.name
+    }); 
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form>
@@ -44,29 +42,14 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <h1>{props.defaultcity}</h1>
-        <ul>
-          <li> <FormattedDate date = {date}/> </li>
-          <li className="text-capitalize">{condition}</li>
-        </ul>
-        <div className="row mt-3">
-          <div className="col-6">
-            <img src={icon} alt={condition}></img>
-            <span className="temperature">{Math.round(temperature)}</span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {Math.round(humidity)} %</li>
-              <li>Wind: {Math.round(wind)} km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfor data = {weatherData} />
+        
       </div>
     );
-  } else {
-    const apiKey = "f3009e4852fa0a079dab291dabf020c4";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultcity}&appid=${apiKey}&units=metric`;
+  }else {
+    const apiKey = "8ca7dd4e61360b90fb66918853670e48";
+    let city = `${props.defaultCity}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
     return "Loading...";
   }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import FormattedDate from "./FormettedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="Weatherinfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          {" "}
+          <FormattedDate date={props.data.date} />{" "}
+        </li>
+        <li className="text-capitalize">{props.data.condition}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img src={props.data.icon} alt={props.data.condition}></img>
+          <span className="temperature">{Math.round(props.data.temperature)}</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {Math.round(props.data.humidity)} %</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
