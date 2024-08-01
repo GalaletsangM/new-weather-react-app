f850bbb7abeba891293770d26bf8e031c3d8a666
GalaletsangM <preciousmafoko@gmail.com>
2024-08-01 15:05:43 -0500

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-
-import "./App.css";
 import Weather from "./Weather";
+import "./App.css";
+
 
 export default function App() {
   return (
     <div className="App">
+
       <div className="container">
-        <Weather defaultcity = "New York"/>
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a href="https://www.linkedin.com/in/galaletsang-precious-mafoko-67a13919a/">
@@ -16,7 +17,6 @@ export default function App() {
           and is open source on{" "}
           <a
             href="https://github.com/GalaletsangM/new-weather-react-app"
-           
           >
             Github
           </a>
--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
-import axios from "axios";
-import WeatherInfor from "./WeatherInfo";
-
-
-export default function Weather(props) {
-  let [weatherData, setWeatherData] = useState({ready : false });
-
-  function handleResponse(response) {
-    setWeatherData ({
-      ready: true,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
-      condition: response.data.weather[0].description,
-      date: new Date(response.data.dt * 1000),
-      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
-      city: response.data.name
-    }); 
-  }
-
-  if (weatherData.ready) {
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-              ></input>
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                className="btn btn-primary w-100"
-                value="Search"
-              ></input>
-            </div>
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            ></input>
           </div>
-        </form>
-        <WeatherInfor data = {weatherData} />
-        
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            ></input>
+          </div>
+        </div>
+      </form>
+      <h1>Katy</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="sunny"
+            ></img>
+            <span className="temperature">36</span>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 55%</li>
+            <li>Wind: 19km/h</li>
+          </ul>
+        </div>
       </div>
-    );
-  }else {
-    const apiKey = "8ca7dd4e61360b90fb66918853670e48";
-    let city = `${props.defaultCity}`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-    return "Loading...";
-  }
+    </div>
+  );
 }
